e55ed9b854e08b2ab41d7f1f48a7a1c9ad36dfd7
Meowhuan <3250315682@qq.com>
2026-02-06 20:47:53 +0800

    update theme

--- src/App.vue
+++ src/App.vue
@@ -112,8 +112,8 @@ const loadGiscus = () => {
   script.setAttribute(
     "data-theme",
     isNight.value
-      ? "https://cdn.jsdelivr.net/gh/meowhuan/Personal_Homepage@main/public/giscus-dark.css"
-      : "https://cdn.jsdelivr.net/gh/meowhuan/Personal_Homepage@main/public/giscus.css"
+      ? "https://cdn.jsdelivr.net/gh/meowhuan/Personal_Homepage@main/public/giscus-dark.css?v=2.0"
+      : "https://cdn.jsdelivr.net/gh/meowhuan/Personal_Homepage@main/public/giscus.css?v=2.0"
   );
   script.setAttribute("data-lang", "zh-CN");
   container.appendChild(script);
@@ -127,8 +127,8 @@ const updateGiscusTheme = () => {
       giscus: {
         setConfig: {
           theme: isNight.value
-          ? "https://cdn.jsdelivr.net/gh/meowhuan/Personal_Homepage@main/public/giscus-dark.css"
-          : "https://cdn.jsdelivr.net/gh/meowhuan/Personal_Homepage@main/public/giscus.css"
+          ? "https://cdn.jsdelivr.net/gh/meowhuan/Personal_Homepage@main/public/giscus-dark.css?v=2.0"
+          : "https://cdn.jsdelivr.net/gh/meowhuan/Personal_Homepage@main/public/giscus.css?v=2.0"
         }
       }
     },
