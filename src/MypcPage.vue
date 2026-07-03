eb4c9603962d904c900d28c74c05dd352299b99c
meowhuan <meowhuan@meowra.cn>
2026-07-03 11:16:46 +0800

    我想可以从只显示硬盘的页面显示更多内容，这仍然是占位

--- src/MypcPage.vue
+++ src/MypcPage.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="disk-page">
-        <h1>Disk Page</h1>
+    <div class="mypc-page">
+        <h1>Mypc Page</h1>
         <p>这是一个占位页面，还没完工</p>
     </div>
 </template>
--- src/mypc-main.js
+++ src/mypc-main.js
@@ -2,8 +2,8 @@ import "@unocss/reset/tailwind.css";
 import "uno.css";
 import "./page-transition.css";
 import { createApp } from "vue";
-import DiskPage from "./DiskPage.vue/index.js";
-import { installPageTransition } from "./page-transition";
+import MypcPage from "./MypcPage.vue/index.js";
+import { installPageTransition } from "./page-transition.js";
 
 installPageTransition();
-createApp(DiskPage).mount("#app");
+createApp(MypcPage).mount("#app");
